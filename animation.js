679765d1f2bab4fd3ef0a37f41228422acba6ee1
MadhushaPrasad <madushaprasad21@gmail.com>
2021-03-21 15:02:09 +0530

    background color change Js

---
+++ animation.js
@@ -0,0 +1,12 @@
+const section = document.querySelector('.page02');
+
+function bgChange() {
+    console.log(section);
+    if (this.scrollY > this.innerHeight / 2) {
+        section.classList.add('bg-active');
+    } else {
+        section.classList.remove('bg-active');
+    }
+}
+
+window.addEventListener('scroll', bgChange);
